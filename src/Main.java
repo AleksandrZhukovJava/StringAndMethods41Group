d17dc282d76adb0fe2e05dd7d519af8bcf6d2b33
Aleksandr <117368114+AleksandrZhukovJava@users.noreply.github.com>
2025-02-01 08:46:44 +0100

    string done (#1)
    Co-authored-by: Александр Жуков <a.zhukov@uzum.com>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String task = "\nTask-"; //ctrl or command
+        System.out.println(task + 1);
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        String fullName2 = firstName.concat(" ").concat(middleName).concat(" ").concat(lastName);
+        String fullName3 = String.format("%s %s %s", firstName, middleName, lastName);
+        String fullName4 = "%s %s %s".formatted(firstName, middleName, lastName);
+        String fullName5 = String.join(" ", firstName, middleName, lastName);
+
+
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println("Ф. И. О. сотрудника — " + fullName2);
+        System.out.println("Ф. И. О. сотрудника — " + fullName3);
+        System.out.println("Ф. И. О. сотрудника — " + fullName4);
+        System.out.println("Ф. И. О. сотрудника — " + fullName5);
+
+
+        System.out.println(task + 2);
+        System.out.printf("Данные Ф. И. О. сотрудника для заполнения отчета — %s%n", fullName.toUpperCase());
+
+        System.out.println(task + 3);
+        fullName = "Иванов Семён Семёнович";
+        String correctedFullName = fullName.replace("ё", "e"); //ctrl + alt <-
+        System.out.printf("Данные Ф. И. О. сотрудника — %s%n", correctedFullName);
     }
 }
