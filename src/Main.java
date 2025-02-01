d331d21d8a6d2e4a2b2da35029285c3cb72a5a63
Александр Жуков <a.zhukov@uzum.com>
2025-02-01 09:41:56 +0100

    methods done

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,58 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        String task = "\nTask-"; //ctrl or command
-        System.out.println(task + 1);
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
-        String fullName = firstName + " " + middleName + " " + lastName;
-        String fullName2 = firstName.concat(" ").concat(middleName).concat(" ").concat(lastName);
-        String fullName3 = String.format("%s %s %s", firstName, middleName, lastName);
-        String fullName4 = "%s %s %s".formatted(firstName, middleName, lastName);
-        String fullName5 = String.join(" ", firstName, middleName, lastName);
+        printTask(1);
+        checkIsLeapYearAndPrint(2025);
+        checkIsLeapYearAndPrint(2028);
 
+        printTask(2);
+        checkValidSystemAndPrint(1, 2024);
+        checkValidSystemAndPrint(0, 2014);
+        checkValidSystemAndPrint(-1, 2014);
+        checkValidSystemAndPrint(0, 2026);
 
-        System.out.println("Ф. И. О. сотрудника — " + fullName);
-        System.out.println("Ф. И. О. сотрудника — " + fullName2);
-        System.out.println("Ф. И. О. сотрудника — " + fullName3);
-        System.out.println("Ф. И. О. сотрудника — " + fullName4);
-        System.out.println("Ф. И. О. сотрудника — " + fullName5);
+        printTask(3);
+        int deliveryDays = getDeliveryDays(60);
+        System.out.println(deliveryDays);
+    }
 
+    public static void checkIsLeapYearAndPrint(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.printf("%s  год — високосный год%n", year);
+        } else {
+            System.out.printf("%s  год — не високосный год%n", year);
+        }
+    }
 
-        System.out.println(task + 2);
-        System.out.printf("Данные Ф. И. О. сотрудника для заполнения отчета — %s%n", fullName.toUpperCase());
+    public static void checkValidSystemAndPrint(int system, int year) {
+        int currentYear = LocalDate.now().getYear();
+        if (system == 0 && year < currentYear) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (system == 0 && year == currentYear) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (system == 1 && year < currentYear) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (system == 1 && year == currentYear) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Не корректные данные");
+        }
+    }
 
-        System.out.println(task + 3);
-        fullName = "Иванов Семён Семёнович";
-        String correctedFullName = fullName.replace("ё", "e"); //ctrl + alt <-
-        System.out.printf("Данные Ф. И. О. сотрудника — %s%n", correctedFullName);
+    public static int getDeliveryDays(int distance) {
+        if (distance >= 0 && distance <= 20) {
+            return 1;
+        } else if (distance > 20 && distance <= 60) {
+            return 2;
+        } else if (distance > 60 && distance <= 100) {
+            return 3;
+        } else {
+            return -1;
+        }
+    }
+
+    public static void printTask(int number) {
+        System.out.println("\nTask " + number);
     }
 }
